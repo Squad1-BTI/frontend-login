4502a1a93b666cf89be05ec70d42a21dafc49007
Robert Santos <robertdahora@gmail.com>
2024-09-02 21:24:30 -0300

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/lib/utils";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center h-full w-full justify-center">
+      <h1>Hello World!</h1>
+    </main>
+  );
+}
