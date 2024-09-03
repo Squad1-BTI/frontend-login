b61e3c3a3a288a190f69fca7e177422f5cbe9612
Miguel <miguelmcontato@gmail.com>
2024-09-03 10:06:56 -0300

    create buttom blue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Buttom from "@/components/buttom";
 import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center h-full w-full justify-center">
       <h1>Hello World!</h1>
+      <Buttom />
+      <Buttom />
     </main>
   );
 }
---
+++ src/components/buttom.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Buttom = () => {
+  return (
+    <button className="text-white p-6 border-2 shadow-md rounded-lg bg-blue-500 hover:bg-blue-700 hover:shadow-cyan-300">Entrar</button>
+  );
+}
+
+export default Buttom;
