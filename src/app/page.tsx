18a50947a2ce31442937569173f3daf7dfb0bbb7
Miguel <miguelmcontato@gmail.com>
2024-09-04 11:30:15 -0300

    define Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { Button } from '@/components/button';
-import { cn } from "@/lib/utils";
+//import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center h-full w-full justify-center">
       <h1>Hello World!</h1>
       <div>
-      <Button variant="destructive" size="lg">
-        Delete
-      </Button>
-      <Button variant="shadow" size="sm">
-        Cancel
-      </Button>
-    </div>
+        <Button>
+          Delete
+        </Button>
+        <Button variant="default" size="sm">
+          Cancel
+        </Button>
+      </div>
     </main>
   );
 }
