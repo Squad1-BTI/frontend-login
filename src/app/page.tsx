bb7b46fa5a5303784b9c1ccf4464ac3a5b45314e
Miguel <miguelmcontato@gmail.com>
2024-09-04 10:46:08 -0300

    install cva and define exemples for button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Buttom from "@/components/buttom";
+import { Button } from '@/components/button';
 import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center h-full w-full justify-center">
       <h1>Hello World!</h1>
-      <Buttom />
+      <div>
+      <Button variant="destructive" size="lg">
+        Delete
+      </Button>
+      <Button variant="shadow" size="sm">
+        Cancel
+      </Button>
+    </div>
     </main>
   );
 }
