c8e7204553d8f42abff874b4feb97ef66fd6b44a
Miguel <miguelmcontato@gmail.com>
2024-09-06 15:08:47 -0300

    new adjustments

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-import { Slot } from '@radix-ui/react-slot';
-
 import { type VariantProps, cva } from 'class-variance-authority';
 
 const buttonVariants = cva(
@@ -35,22 +33,18 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
+    VariantProps<typeof buttonVariants> {}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
+  ({ className, variant, size, ...props }, ref) => {
     return (
-      <Comp
+      <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-        {...props}
-      />
+        {...props}>
+      </button>
     );
   }
 );
-Button.displayName = 'Button';
 
 export { Button, buttonVariants };
