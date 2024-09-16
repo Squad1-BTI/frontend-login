018df07605dae3ed3c519b1aea21a4b9865dcdc0
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-09-16 14:06:32 -0300

    change layout login

--- src/app/login/login.tsx
+++ src/app/login/login.tsx
@@ -14,27 +14,27 @@ function Login() {
           };
 
           return (
-            <div className="flex flex-col items-center h-screen w-full justify-center px-4">
-              <img className="w-1/5" src="/images/logo.svg" alt="Logo" />
+            <div className="flex flex-col items-center h-screen w-full justify-center space-y-4 px-4">
+              <img className="w-96 " src="/images/logo.svg" alt="Logo" />
                   <h1 className="text-4xl font-bold mb-4">Login</h1>
                   <p className="text-textGray mb-6">Insira suas credenciais para acessar sua conta</p>
               <form onSubmit={handleSubmit} className="w-full max-w-sm">
-                      <label className="font-bold" htmlFor="email">Email</label>
+                      <label className="font-bold flex mb-2" htmlFor="email">Email</label>
                       <Input 
                           type="email"
                           placeholder="Seu email"
-                          className="mb-4"
+                          className="mb-6 "
                       />
 
-                      <label className="font-bold"  htmlFor="password">Senha</label>
+                      <label className="font-bold flex mb-2"  htmlFor="password">Senha</label>
                     <Input
                           type="password"
                           placeholder="Sua senha"
                           togglePassword={true}
-                          className="mb-4"
+                          className="mb-2"
                           />
                       <div className="flex justify-between items-center mb-4">
-                          <a href="/esqueceu-senha" className="text-right w-full text-sm text-primary hover:underline">
+                          <a href="/esqueceu-senha" className="text-right my-3 w-full text-sm text-primary hover:underline">
                               Esqueceu sua senha?
                           </a>
                 </div>
@@ -43,7 +43,7 @@ function Login() {
                       </Button>
                       <div className="text-sm text-center">
                           <span className="text-textGray">Não tem uma conta?</span>
-                          <a href="/criar-conta" className="text-primary hover:underline ml-1">
+                          <a href="/criar-conta" className="text-primary hover:underline ml-1 ">
                               Crie a sua conta aqui
                           </a>
                 </div>
--- src/components/UI/input.tsx
+++ src/components/UI/input.tsx
@@ -3,33 +3,6 @@
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
-import { type VariantProps, cva } from 'class-variance-authority';
-
-const inputVariants = cva(
-  'flex flex-col gap-2',
-  {
-    variants: {
-      variant: {
-        default: 'text-gray-800 bg-secundary peer block w-full border-1 px-2 py-2 focus:outline-none placeholder:text-place  placeholder:text-sm ',
-      },
-      inputSize: {
-        default: 'min-w- text-lg',
-        sm: 'w-18 text-xl',
-        lg: 'w-54 text-xl',
-      },
-      rounded: {
-        default: 'rounded-lg',
-        md: 'rounded-md',
-        none: 'rounded-none',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-      inputSize: 'default',
-      rounded: 'default',
-    },
-  }
-);
 
 export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'>{
   togglePassword?: boolean; 
@@ -44,22 +17,22 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     };
 
     return (
-      <div className="relative flex items-center m-4 w-full">
+      <div className="relative flex items-center m- w-full">
         <input
-          className={cn('min-w-[440px]:', className , togglePassword && 'pr-10')}
+          className={cn('min-w-[440px]:text-gray-800 bg-secundary peer block w-full border px-2 py-2 focus:outline-none placeholder:text-place placeholder:text-sm rounded-lg', className , togglePassword && 'pr-10')}
           ref={ref}
           type={togglePassword && !showPassword ? 'password' : 'text'}
           {...props}
         />
         {togglePassword && (
           <span
-            className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            className="absolute right-2 transform -translate-y-1/2 cursor-pointer"
             onClick={handleToggle}
           >
             {showPassword ? (
-              <EyeIcon className="w-5 h-5 text-gray-400"/>
+              <EyeIcon className="w-5 text-gray-400"/>
             ) : (
-              <EyeSlashIcon className="w-5 h-5 text-gray-400"/>
+              <EyeSlashIcon className="w-5  h-5 text-gray-400"/>
             )}
           </span>
         )}
@@ -70,4 +43,4 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 
 Input.displayName = 'Input';
 
-export { Input, inputVariants };
+export { Input };
