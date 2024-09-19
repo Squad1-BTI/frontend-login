2082ad2445796fe238f9fe8543eb6c9d44c048a5
Miguel <miguelmcontato@gmail.com>
2024-09-19 17:53:44 -0300

    prepare to merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-
+import { Button } from '@/components/UI/button';
+import { Input } from '@/components/UI/input';
 
 
 export default function Home() {
@@ -18,34 +19,3 @@ export default function Home() {
     </main>
   );
 }
-
-/*
-import { Button } from '@/components/UI/button';
-import { Input } from '@/components/UI/input';
-
-export default function Home() {
-  return (
-    <main className="flex flex-col items-center h-full w-full justify-center">
-      <h1 className="text-primary">Hello World!</h1>
-      <div>
-        <Button>
-          Delete
-        </Button>
-        <Button variant="default" size="sm">
-          Cancel
-        </Button>
-        <Input
-          type="email"
-          placeholder="Seu email"
-         
-        />
-        <Input
-          type="password"
-          placeholder="Sua senha"
-           togglePassword={true}
-        />
-      </div>
-    </main>
-  );
-}
-*/
